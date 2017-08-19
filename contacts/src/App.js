c0eeddc16511496f201c92bc8fb0785ca6876499
rogeralbinoi <roger.albino.i@gmail.com>
2017-08-19 19:55:08 -0300

    add: remove contact from api

--- contacts/src/App.js
+++ contacts/src/App.js
@@ -12,6 +12,8 @@ class App extends Component {
     this.setState( (state) => ({
       contacts: state.contacts.filter((c) => c.id !== contact.id)
     }))
+
+    ContactsAPI.remove(contact)
   }
   
   componentDidMount() {
